5bcaa5c35192b977d0742b3e705ee1880125e806
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-08-23 18:55:10 +0500

    3-set up PostgresSQL db connection

---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+# Create an SQLAlchemy engine to connect to the PostgresSQL database.
+# The URL format is: "postgresql+psycopg2://username:password@host:port/database_name"
+engine = create_engine(
+    "postgresql+psycopg2://postgres:asliddin@delivery_project:5432/asliddin",
+    echo=True  # Setting this to True will print SQL statements for testing purposes. Not recommend for deployment
+)
+
+# Create a base class for declarative SQLAlchemy models.
+Base = declarative_base()
+
+# Create a session-maker to create database sessions.
+Session = sessionmaker()
+
+# Here, you've set up the basic components for interacting with the PostgresSQL database:
+# - `engine` to connect to the database
+# - `Base` as the base class for declarative models
+# - `Session` to create database sessions
+
+# You can now define your SQLAlchemy models using the `Base` class, and use the `Session` to interact with the database.
